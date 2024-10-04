0e79bc23cc0c962aad5a6a5d959a86c1d570a937
altaf2004 <altafhashmi2004@gmail.com>
2024-10-04 08:31:06 +0000

    js created

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+//Dom Selection
+//First target to input
+const inputE1 = document.querySelector('input');
+
+//add to focus input
+document.addEventListener('keypress', function () {
+  consol.console.log('wah');
+});
